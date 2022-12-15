833eaddf67959ecf51dd9ba003501504a0069779
damiang <gildeadamian@gmail.com>
2022-12-15 16:38:30 +0000

    Completed 2B

--- task2b/index.js
+++ task2b/index.js
@@ -1,54 +1,59 @@
+// Identify all the files that contain errors
+// Find where the potential errors are - see error messages
+// Identify each specific error
+// test the specific piece of code entirely / console log it
+// Find out what the error may be linked to i.e. just this file or used elsewhere?
+// Screenshot the code and save as documentation as you go with comments
+// Run tests, console logs, and fix each error
+
 function add(numb1, numb2) {
-  let num1 = String(numb1);
-  return num1 + numb2;
+  return numb1 + numb2;
 }
 
 function multiply(num1, operator, num2) {
-  num1, operator, num2;
+  if (operator === "*") return num1 * num2;
 }
 
 function hasPets(hasPet, firstName, lastName) {
-  if (hasPet === "true") {
+  if (hasPet === true) {
     return firstName + " " + lastName + " really does have a nice pet";
   } else {
-    return;
-    firstName + " " + lastName + " " + "has no pets";
+    return firstName + " " + lastName + " " + "has no pets";
   }
 }
 
 let myAccountNumbers = {
-  account1: "220",
+  account1: 220,
   account2: 144,
   account3: 12,
   account4: 1443,
 };
 
 function addsBalanceOfAccounts() {
-  let firstAccount = myAccountNumbers[1];
-  let secondAccount = myAccountNumbers["account3"];
+  let firstAccount = myAccountNumbers.account2;
+  let secondAccount = myAccountNumbers.account3;
   return firstAccount + secondAccount;
 }
 
 function calculator(firstNumber, operator, secondNumber) {
   if (operator === "+") {
     let answer = firstNumber + secondNumber;
-    return;
+    return answer;
   }
   if (operator === "-") {
     let answer = firstNumber - secondNumber;
-    return;
+    return answer;
   }
   if (operator === "*") {
     let answer = firstNumber * secondNumber;
-    return;
+    return answer;
   }
   if (operator === "/") {
     let answer = firstNumber / secondNumber;
-    return;
+    return answer;
   } else {
     let answer = "Invalid inputs. Try again!";
-    return;
-    answer;
+    return answer;
   }
 }
 
